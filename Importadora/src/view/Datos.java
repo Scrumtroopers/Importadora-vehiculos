13a7eda2f17ce58b0888fb5899ac168c0217ac50
cristhianzb <cristhianzentenob@gmail.com>
2017-05-31 01:22:56 -0400

    Datos
    Restringir ingreso de datos en los campos de entrada

---
+++ Importadora/src/view/Datos.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JTextField;
+import javax.swing.JFrame; 
+
+/**
+ *
+ * @author Cristhian
+ */
+public class Datos extends JFrame{
+    	
+    //admite solo numeros decimales
+    private void SDec(JTextField a){
+	        a.addKeyListener(new KeyAdapter(){
+                    @Override
+	            public void keyTyped(KeyEvent e){
+	                  char c=e.getKeyChar();
+	                  if(!Character.isDigit(c)&&c!='.'){
+	                      getToolkit().beep();
+	                      e.consume();
+	                  }
+	            }
+	        });
+    }
+    
+    //admite solo numeros enteros
+    private void SInt(JTextField a){
+	        a.addKeyListener(new KeyAdapter(){
+                    @Override
+	            public void keyTyped(KeyEvent e){
+	                  char c=e.getKeyChar();
+	                  if(!Character.isDigit(c)){
+	                      getToolkit().beep();
+	                      e.consume();
+	                  }
+	            }
+	        });
+    }
+    
+    
+    //admite solo letras
+    private void SLetra(JTextField a){
+	        a.addKeyListener(new KeyAdapter(){
+                    @Override
+	            public void keyTyped(KeyEvent e){
+	                  char c=e.getKeyChar();
+	                  if(!Character.isLetter(c)){
+	                      getToolkit().beep();
+	                      e.consume();
+	                  }
+	            }
+	        });
+    }
+    
+    
+    //admite solo numeros enteros
+    private void SMin(JTextField a){
+	        a.addKeyListener(new KeyAdapter(){
+                    @Override
+	            public void keyTyped(KeyEvent e){
+	                  char c=e.getKeyChar();
+	                  if(!Character.isLowerCase(c)){
+	                      getToolkit().beep();
+	                      e.consume();
+	                  }
+	            }
+	        });
+    }
+    
+    //admite solo numeros enteros
+    private void SMayus(JTextField a){
+	        a.addKeyListener(new KeyAdapter(){
+                    @Override
+	            public void keyTyped(KeyEvent e){
+	                  char c=e.getKeyChar();
+	                  if(!Character.isUpperCase(c)){
+	                      getToolkit().beep();
+	                      e.consume();
+	                  }
+	            }
+	        });
+    }
+    
+    
+    private double getDouble(JTextField tf){
+            double numero=0.0;
+            String num = tf.getText();
+
+         try {numero = Double.parseDouble(num);
+
+         } catch( Exception e){
+             System.out.println("numero incorrecto");
+         }
+         return numero;
+
+    }
+    
+    private int getInt(JTextField tf){
+            int numero=0;
+            String num = tf.getText();
+
+         try {numero = Integer.parseInt(num);
+
+         } catch( Exception e){
+             System.out.println("numero incorrecto");
+         }
+         return numero;
+
+    }
+}
