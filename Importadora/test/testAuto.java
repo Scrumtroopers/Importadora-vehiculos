85452f6f2a26507ecd1c7cfb8aee0e30dc2cba5f
pameliuski <pamela.garces.official@gmail.com>
2017-05-31 22:53:08 -0400

    tests basics para auto, factura, pedido

---
+++ Importadora/test/testAuto.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import importadora.autos.*;
+/**
+ *
+ * @author pameliuski
+ */
+public class testAuto {
+
+    /*  el VIN debe ser 17 caracteres. Los primero 8 deben ser letras mayusculas, y los restantes 9
+        deben ser digitos. En ese orden.
+    */
+    @Test
+    public void validarVINTest() {
+        String VIN = "ABCDEFGH123456789";
+        Auto auto = new Auto(VIN,"","",600,0,0);
+        assertEquals(true, auto.validarVIN());
+       
+        String VIN2 = "ABCDEFGH";
+        Auto auto2 = new Auto(VIN2,"","",600,0,0);
+        assertEquals(false, auto2.validarVIN());
+         
+        String VIN3 = "A1111111123456789";
+        Auto auto3 = new Auto(VIN3,"","",600,0,0);
+        assertEquals(false, auto3.validarVIN());
+        
+        String VIN4 = "A9C7G8H123456FGH";
+        Auto auto4 = new Auto(VIN4,"","",600,0,0);
+        assertEquals(false, auto4.validarVIN()); 
+    }
+    
+    //subTotal de cada auto = precioVenta + precio extra (de accesorios) + garantia
+    @Test
+    public void calcularSubTotalTest() {
+        double precioVenta = 1000;
+        double gastosExtras = 200;
+        double garantia = 150;
+        Auto auto = new Auto();
+        auto.setPrecioVenta(precioVenta);
+        auto.setGastosExtras(gastosExtras);
+        auto.setGarantia(garantia);
+        double subtotal = auto.calcularSubTotal();
+        assertEquals(1350,subtotal,0.001);
+    }
+}
---
+++ Importadora/test/testPedido.java
@@ -0,0 +1,74 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import importadora.autos.*;
+/**
+ *
+ * @author pameliuski
+ */
+public class testPedido {
+
+    // Si al agregar el auto el VIN no es valido, no se agrega
+    @Test 
+    public void agregarAutoTest() {
+        double precioVenta = 1000;
+        double gastosAccesorios = 200;
+        double garantia = 200;
+        String VIN = "ABCDEFGH123456789";
+        Auto car = new Auto();
+        Pedido pedido = new Pedido();
+
+        car.setPrecioVenta(precioVenta);
+        car.setGastosExtras(gastosAccesorios);
+        car.setGarantia(garantia);
+        car.setVIN(VIN);
+        pedido.agregarAuto(car);
+        
+        Auto car2 = new Auto("AABBCDEF123456677","","",500,500,500);
+        pedido.agregarAuto(car2);
+        
+        Auto car3 = new Auto("AABB123456688CDEF","","",500,500,500); // no es VIN valido, entonces no se considera
+        pedido.agregarAuto(car3);
+        
+        assertEquals(2,pedido.getAutos().size());
+    }
+    
+    @Test 
+    public void obtenerAutoTest() {
+        double precioVenta = 1000;
+        double gastosAccesorios = 200;
+        double garantia = 200;
+        String VIN = "ABCDEFGH123456789";
+        Auto car = new Auto();
+        Pedido pedido = new Pedido();
+
+        car.setPrecioVenta(precioVenta);
+        car.setGastosExtras(gastosAccesorios);
+        car.setGarantia(garantia);
+        car.setVIN(VIN);
+        pedido.agregarAuto(car);
+        
+        Auto car2 = new Auto("AABBCDEF123456677","","",500,500,500);
+        pedido.agregarAuto(car2);
+       
+        assertEquals(car2,pedido.obtenerAuto("AABBCDEF123456677"));
+        assertNotSame(car2,pedido.obtenerAuto(VIN));
+    }
+    
+    @Test 
+    public void calcularSubTotalAutosTest() {
+
+        Pedido pedido = new Pedido();
+        
+        Auto car = new Auto("ABCDEFGH123456789","","",1000,200,200);
+        Auto car2 = new Auto("AABBCDEF123456677","","",200,150,200);
+        
+        pedido.agregarAuto(car);
+        pedido.agregarAuto(car2);
+        //actualiza el subtotal de todo el pedido
+        double subtotalAutos = pedido.calcularSubTotalAutos();
+        
+        assertEquals(1950,subtotalAutos,0.001);
+    }
+    
+}
+
