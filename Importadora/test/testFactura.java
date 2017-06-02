0a31636605869af1955a4abc6f268df694077def
cachnd <alex.cachnd@gmail.com>
2017-06-01 22:04:39 -0400

    test codigo de control factura

--- Importadora/test/testFactura.java
+++ Importadora/test/testFactura.java
@@ -60,12 +60,11 @@ public class testFactura {
     @Test
     public void codigoControlTest() {
         CodControl cc = new CodControl();
-        String[] factura ={"7904006306693","876814","1665979","2008/05/19","35958,60","zZ7Z]xssKqkEf_6K9uH(EcV+%x+u[Cca9T%+_$kiLjT8(zr3T9b5Fx2xG-D+_EBS","27773","7904006306693zZ787681455Z]xssKqk166597949Ef_6K9uH2008051967(EcV+%x+3595999u[Cc"},
-                facturat = {"7904006306693","876814","1665979","2008/05/19","35958,60","zZ7Z]xssKqkEf_6K9uH(EcV+%x+u[Cca9T%+_$kiLjT8(zr3T9b5Fx2xG-D+_EBS","27773","7904006306693zZ787681455Z]xssKqk166597949Ef_6K9uH2008051967(EcV+%x+3595999u[Cc"},
-                factura2 ={"8004005263848","673173","1666188","2008/08/10","51330,00","PNRU4cgz7if)[tr#J69j=yCS57i=uVZ$n@nv6wxaRFP+AUf*L7Adiq3TT[Hw-@wt","69999","8004005263848PNRU4cg67317336z7if)[tr#J16661882369j=yCS57i2008081050=uVZ$n@nv65133016wxaRFP+AUf"},
-                factura3 ={"7904006098968","165657","1666615","2008/06/30","96459,00","m3dcSc)Dg#SN}prtK=9xn[m+pgAxL%N67G}QfwNZM+)IzCnvP$T*qjEKhmJnaDHm","62652","7904006098968m3dcSc)16565710Dg#166661503SN}prtK2008063010=9xn[m9645923+pg"},
-                factura4 ={"7904004313753","826384","1666982","2008/06/22","61102,70","Ebs[$c2d2NCg5FYj@6nU5y##a5d]eDVz%]xW6bzcd}Kd)\\w\\=c+)dZHneF#bqVL@","04261","7904004313753E82638481bs[$c1666982202d22008062277NCg5FYj6110385@6"},
-                factura5 ={"1904008691195","978256","0","2008/02/01","26006,00","pPgiFS%)v}@N4W3aQqqXCEHVS2[aDw_n%3)pFyU%bEB9)YXt%xNBub4@PZ4S9)ct","21885","1904008691195pPg97825622iF047S%)v}@N4W20080201233aQqqXCEH2600627VS2[aD"};
+        String[] factura ={"7904006306693","876814","1665979","2008/05/19","35958,60","zZ7Z]xssKqkEf_6K9uH(EcV+%x+u[Cca9T%+_$kiLjT8(zr3T9b5Fx2xG-D+_EBS","27773","7904006306693zZ787681455Z]xssKqk166597949Ef_6K9uH2008051967(EcV+%x+3595999u[Cc","15847127","ySxN","7B-F3-48-A8"},
+                factura2 ={"8004005263848","673173","1666188","2008/08/10","51330,00","PNRU4cgz7if)[tr#J69j=yCS57i=uVZ$n@nv6wxaRFP+AUf*L7Adiq3TT[Hw-@wt","69999","8004005263848PNRU4cg67317336z7if)[tr#J16661882369j=yCS57i2008081050=uVZ$n@nv65133016wxaRFP+AUf","12484570","ld/Q","2C-98-0E-96"},
+                factura3 ={"7904006098968","165657","1666615","2008/06/30","96459,00","m3dcSc)Dg#SN}prtK=9xn[m+pgAxL%N67G}QfwNZM+)IzCnvP$T*qjEKhmJnaDHm","62652","7904006098968m3dcSc)16565710Dg#166661503SN}prtK2008063010=9xn[m9645923+pg","15719356","xzky","AB-DD-0E-9B"},
+                factura4 ={"7904004313753","826384","1666982","2008/06/22","61102,70","Ebs[$c2d2NCg5FYj@6nU5y##a5d]eDVz%]xW6bzcd}Kd)\\w\\=c+)dZHneF#bqVL@","04261","7904004313753E82638481bs[$c1666982202d22008062277NCg5FYj6110385@6","23846491","1QzvR","04-FF-52-67-CD"},
+                factura5 ={"1904008691195","978256","0","2008/02/01","26006,00","pPgiFS%)v}@N4W3aQqqXCEHVS2[aDw_n%3)pFyU%bEB9)YXt%xNBub4@PZ4S9)ct","21885","1904008691195pPg97825622iF047S%)v}@N4W20080201233aQqqXCEH2600627VS2[aD","15810900","yK5K","62-12-AF-1B"};
                 
         cc.setNroAut(factura[0]);
         cc.setNroFact(factura[1]);
@@ -75,7 +74,9 @@ public class testFactura {
         cc.setLlave(factura[5]);
         assertEquals(factura[6], cc.getVerhoeff()); 
         assertEquals(factura[7], cc.getCadena());
-        
+        assertEquals(factura[8], cc.getSumatoriaProductos());
+        assertEquals(factura[9], cc.getBase64());
+        assertEquals(factura[10], cc.getCodigoControl());
         
         cc.setNroAut(factura2[0]);
         cc.setNroFact(factura2[1]);
@@ -85,6 +86,9 @@ public class testFactura {
         cc.setLlave(factura2[5]);
         assertEquals(factura2[6], cc.getVerhoeff());
         assertEquals(factura2[7], cc.getCadena());
+        assertEquals(factura2[8], cc.getSumatoriaProductos());
+        assertEquals(factura2[9], cc.getBase64());
+        assertEquals(factura2[10], cc.getCodigoControl());
         
         cc.setNroAut(factura3[0]);
         cc.setNroFact(factura3[1]);
@@ -94,6 +98,8 @@ public class testFactura {
         cc.setLlave(factura3[5]);
         assertEquals(factura3[6], cc.getVerhoeff());
         assertEquals(factura3[7], cc.getCadena());
+        assertEquals(factura3[8], cc.getSumatoriaProductos());
+        assertEquals(factura3[9], cc.getBase64());
         
         cc.setNroAut(factura4[0]);
         cc.setNroFact(factura4[1]);
@@ -103,6 +109,10 @@ public class testFactura {
         cc.setLlave(factura4[5]);
         assertEquals(factura4[6], cc.getVerhoeff());
         assertEquals(factura4[7], cc.getCadena());
+        assertEquals(factura4[8], cc.getSumatoriaProductos());
+        assertEquals(factura4[9], cc.getBase64());
+        assertEquals(factura4[10], cc.getCodigoControl());
+        
         
         cc.setNroAut(factura5[0]);
         cc.setNroFact(factura5[1]);
@@ -112,6 +122,9 @@ public class testFactura {
         cc.setLlave(factura5[5]);
         assertEquals(factura5[6], cc.getVerhoeff());
         assertEquals(factura5[7], cc.getCadena());
+        assertEquals(factura5[8], cc.getSumatoriaProductos());
+        assertEquals(factura5[9], cc.getBase64());
+        assertEquals(factura5[10], cc.getCodigoControl());
         
     }
 
