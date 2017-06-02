6d5ca40e1257fd7af662c94300ba32623328d2b9
Ariel Choquetopa <crashariel@hotmail.com>
2017-06-02 00:46:31 -0400

    agregado de funcion a la base de datos

--- Importadora/src/model/FuncionesInventario.java
+++ Importadora/src/model/FuncionesInventario.java
@@ -14,10 +14,25 @@ import java.sql.Statement;
  * @author ariel
  */
 public class FuncionesInventario extends FuncionesBaseDeDatos {
-    
-    public FuncionesInventario(Connection conexion, Statement statement){
+
+    private FuncionesAutomovil funcionAuto;
+
+    public FuncionesInventario(Connection conexion, Statement statement) {
         super(conexion, statement);
+        funcionAuto = new FuncionesAutomovil(conexion, statement);
     }
-    
-    
+
+    /* public boolean agregarVehiculoInventario(String fabricante, String combustible, String tipo_caja, String modelo, String estado_vehiculo,
+                                            String color, double precio, int nr_chasis, String descripcion, double garantia){
+        
+    }*/
+    public boolean agregarVehiculoInventario(Auto auto, String almacen, int cantidad, String observaciones) {
+        boolean valido = funcionAuto.registrarAuto(auto);
+        boolean insertado = false;
+        if (valido) {
+            insertado = this.insertarEnTabla("inventario", new String[]{"almacen_id", "vehiculo_id", "cantidad", "observaciones"}, new Object[]{funcionAuto.getIdVehiculo(cantidad),
+                                            1, cantidad, observaciones});
+        }
+        return insertado;
+    }
 }
